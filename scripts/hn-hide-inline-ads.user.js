bd3ae21ccf8b5351ac3a773b80267868f5a0a6db
Mohamed Elashri <mail@elashri.com>
2024-08-09 01:50:24 -0400

    Update hn-hide-inline-ads.user.js

--- scripts/hn-hide-inline-ads.user.js
+++ scripts/hn-hide-inline-ads.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hacker News - Hide Inline Ads
 // @namespace    https://github.com/MohamedElashri/HN
-// @version      1.0
+// @version      1.1
 // @description  Hides inline ads on Hacker News
 // @author       melashri
 // @match        https://news.ycombinator.com/*
@@ -13,31 +13,30 @@
 (function() {
     'use strict';
 
-    // function to remove inline ads
+    // Function to remove inline ads
     function hideInlineAdsOnHN() {
         // Select all posts (rows with class "athing")
-        let things = document.querySelectorAll("tr.athing");
+        const things = document.querySelectorAll("tr.athing");
 
         // Iterate over each post
-        for(let i = 0; i < things.length; i++) {
-            // Ensure the nextSibling exists and is the expected subtext row
-            if(things[i].nextSibling && things[i].nextSibling.querySelector("td.subtext")) {
-                // Check if the subtext row has exactly two child elements
-                if(things[i].nextSibling.querySelector("td.subtext").childElementCount == 2) {
-                    // Hide the post row
-                    things[i].style.display = "none";
-                    // Hide the subtext row
-                    things[i].nextSibling.style.display = "none";
+        things.forEach(thing => {
+            const subtextRow = thing.nextSibling;
+            const subtext = subtextRow ? subtextRow.querySelector("td.subtext") : null;
 
-                    // Check and hide the additional related element, which could be spacing or further details
-                    // Make sure to safely check for the existence of next siblings to avoid errors
-                    let possibleAdDetailsRow = things[i].nextSibling.nextSibling;
-                    if(possibleAdDetailsRow && possibleAdDetailsRow.nextSibling) {
-                        possibleAdDetailsRow.nextSibling.style.display = "none";
-                    }
+            // Check if the subtext row has exactly two child elements
+            if (subtext && subtext.childElementCount === 2) {
+                // Hide the post row
+                thing.style.display = "none";
+                // Hide the subtext row
+                subtextRow.style.display = "none";
+
+                // Check and hide the additional related element, which could be spacing or further details
+                const possibleAdDetailsRow = subtextRow.nextSibling;
+                if (possibleAdDetailsRow) {
+                    possibleAdDetailsRow.style.display = "none";
                 }
             }
-        }
+        });
     }
 
     // Remove inline ads
